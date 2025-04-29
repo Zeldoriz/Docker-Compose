500f9ba3a53c9357bf583afbd944d7117a0a5697
Andrew Tian <exparados@gmail.com>
2025-04-29 17:01:31 +0800

    updated migration scripts, migration knexfile env

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -1,4 +1,10 @@
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({ path: path.join(__dirname + '../../../.env') });
+
 import type { Knex } from 'knex';
+
+console.log(process.env);
 
 export default {
   development: {
@@ -9,6 +15,18 @@ export default {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       port: Number(process.env.DB_PORT)
-    }
+    },
+    migrations: './migrations'
+  },
+  migrate: {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT)
+    },
+    migrations: './migrations'
   }
 } as { [key: string]: Knex.Config };
---
+++ src/db/migrations/20250429081840_drop_products.ts
@@ -0,0 +1,14 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('products');
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.createTable('products', (table: Knex.CreateTableBuilder) => {
+    table.increments('product_id').primary();
+    table.string('product_name').notNullable();
+    table.string('product_description').notNullable();
+    table.decimal('product_price', 10, 2).notNullable();
+  });
+}
