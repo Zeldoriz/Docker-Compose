c52ed94217563d5c1573741cb1220aee91e1e4c4
Andrew Tian <exparados@gmail.com>
2025-04-29 12:50:13 +0800

    added abstraction with .env in docker-compose, rookie mistake of pushing docker-compose without environment variables

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -4,7 +4,7 @@ export default {
   development: {
     client: 'mysql',
     connection: {
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
